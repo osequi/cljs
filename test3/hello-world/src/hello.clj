a08228194620c64f8cd59b7df0ae4f3f27a9c2e6
osequi <cs@osequi.com>
2021-04-04 18:27:09 +0300

    All works

--- test3/hello-world/src/hello.clj
+++ test3/hello-world/src/hello.clj
@@ -1,5 +1,7 @@
 (ns hello
   (:require [hello-time :as ht]))
 
-(defn run [opts]
+(defn run
+  "Displays a hello world message" 
+  []
   (println "Hello world, the time is" (ht/time-str (ht/now))))
